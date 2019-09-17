8746f0cf7fbb9e36453cfa1c0708fcbed6b2c915
NiveditaRao <nivelixir@gmail.com>
2019-09-16 20:31:28 -0700

    adding files

---
+++ models.py
@@ -0,0 +1,23 @@
+from database import Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.types import DateTime
+
+class Items(Base):
+    """
+    Items table
+    """
+    __tablename__ = 'items'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(256))
+    quantity = Column(Integer)
+    description = Column(String(256))
+    date_added = Column(DateTime())
+
+    @property
+    def serialize(self):
+        return{
+            'name': self.name,
+            'quantity': self.quantity,
+            'description': self.description,
+            'date_added' : self.date_added
+        }
